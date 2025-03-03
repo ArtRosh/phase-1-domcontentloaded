df935b836ebe65d7ec590d62bb19f47ae87902ba
ArtRosh <artem.rosh17@gmail.com>
2025-03-02 21:18:20 -0800

    lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+//ensure the DOM is fully loaded before running the script
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("p").textContent = "This is really cool!";
+});
